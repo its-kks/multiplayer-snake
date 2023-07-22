2ea633cf0259c1950fb3da1d340493ebdf2c727c
KKS <itssocialkks@gmail.com>
2023-07-22 14:39:05 +0530

    Setting food and snake color

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const FOOD_COLOR = '#e66916';
 const gameScreen = document.querySelector('#gameScreen');
 let canvas, ctx;
 
+const gameState={
+    player:{
+        pos:{
+            x:3,
+            y:10
+        },
+        vel:{
+            x:1,
+            y:0
+        },
+        snake:[
+            {x:1,y:10},
+            {x:2,y:10},
+            {x:3,y:10},
+        ]
+    },
+    food:{
+        x:7,
+        y:7
+    },
+    gridsize:20
+
+}
+
 const init = () => {
     canvas = document.querySelector('#canvas');
     ctx = canvas.getContext('2d');
@@ -22,3 +46,30 @@ const init = () => {
 }
 
 init();
+
+const paintGame = (state)=>{//state -> gameState
+    ctx.fillStyle=BG_COLOR;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    const food = state.food;
+    const gridsize = state.gridsize;
+    const size = canvas.width/gridsize;
+
+    ctx.fillStyle = FOOD_COLOR;
+    //ith cell but we have to multiply cell length to get pixel
+    ctx.fillRect(food.x*size,food.y*size,size,size);
+
+    paintPlayer(state.player,size,SNAKE_COLOR);
+}
+
+const paintPlayer = (playState,size,color)=>{
+    const snake=playState.snake;
+
+    ctx.fillStyle = color;
+    for(let cell of snake){
+        ctx.fillRect(cell.x*size,cell.y*size,size,size);
+    }
+
+}
+
+paintGame(gameState);
