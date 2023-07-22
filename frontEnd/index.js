906763be95d9b1de91f466b8f13750b00c8b421e
KKS <itssocialkks@gmail.com>
2023-07-22 17:40:44 +0530

    Connecting client to server

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -2,32 +2,29 @@ const BG_COLOR = '#231f20';
 const SNAKE_COLOR = '#c2c2c2';
 const FOOD_COLOR = '#e66916';
 
+// // Making a client instance
+const socket = io('http://localhost:3000');
+socket.on('init',handleInit);
+
 const gameScreen = document.querySelector('#gameScreen');
 let canvas, ctx;
 
-const gameState={
-    player:{
-        pos:{
-            x:3,
-            y:10
-        },
-        vel:{
-            x:1,
-            y:0
-        },
-        snake:[
-            {x:1,y:10},
-            {x:2,y:10},
-            {x:3,y:10},
+const gameState = {
+    player: {
+        pos: { x: 3, y: 10 },
+        vel: { x: 1, y: 0 },
+        snake: [
+            { x: 1, y: 10 },
+            { x: 2, y: 10 },
+            { x: 3, y: 10 },
         ]
     },
-    food:{
-        x:7,
-        y:7
+    food: {
+        x: 7,
+        y: 7
     },
-    gridsize:20
-
-}
+    gridsize: 20
+};
 
 const init = () => {
     canvas = document.querySelector('#canvas');
@@ -43,33 +40,41 @@ const init = () => {
     document.addEventListener('keydown', (e) => {
         console.log(e.key);
     });
-}
+};
 
 init();
 
-const paintGame = (state)=>{//state -> gameState
-    ctx.fillStyle=BG_COLOR;
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+const paintGame = (state) => { //state -> gameState
+    ctx.fillStyle = BG_COLOR;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     const food = state.food;
     const gridsize = state.gridsize;
-    const size = canvas.width/gridsize;
+    const size = canvas.width / gridsize;
 
     ctx.fillStyle = FOOD_COLOR;
-    //ith cell but we have to multiply cell length to get pixel
-    ctx.fillRect(food.x*size,food.y*size,size,size);
+    // ith cell but we have to multiply cell length to get pixel
+    ctx.fillRect(food.x * size, food.y * size, size, size);
 
-    paintPlayer(state.player,size,SNAKE_COLOR);
-}
+    paintPlayer(state.player, size, SNAKE_COLOR);
+};
 
-const paintPlayer = (playState,size,color)=>{
-    const snake=playState.snake;
+const paintPlayer = (playState, size, color) => {
+    const snake = playState.snake;
 
     ctx.fillStyle = color;
-    for(let cell of snake){
-        ctx.fillRect(cell.x*size,cell.y*size,size,size);
+    for (let cell of snake) {
+        ctx.fillRect(cell.x * size, cell.y * size, size, size);
     }
-
-}
+};
 
 paintGame(gameState);
+
+function handleInit(msg) {
+    console.log(msg);
+}
+
+function handleGameState(gameState){
+    gameState = JSON.parse(gameState);
+    requestAnimationFrame(()=>paintGame(gameState));
+}
